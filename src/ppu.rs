99d2c6c8c9554a5a76e15d59c4e4e73b939b8969
Nathan Leiby <nathanleiby@gmail.com>
2024-12-09 14:28:11 -0800

    chore: add tests for addr register behavior

--- src/ppu.rs
+++ src/ppu.rs
@@ -109,11 +109,16 @@ impl AddrRegister {
     }
 
     /// increment is called when there's a read/write to the corresponding `data` register
+    // TODO: Should `inc` explicitly only support the valid args of 1 or 32?
     fn increment(&mut self, inc: u8) {
         let base = self.get();
         let updated = base.wrapping_add(inc as u16);
 
         self.set(updated & ADDR_REGISTER_MIRROR_MASK);
+    }
+
+    pub fn reset_to_hi_bit(&mut self) {
+        self.is_lo_byte = false;
     }
 }
 
@@ -149,17 +154,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_addr_register_mem_writes() {
+    fn test_addr_register_external_writes_and_increment() {
         let mut ar = AddrRegister::new();
         assert_eq_hex!(ar.get(), 0);
 
+        // hi bit
         ar.external_write(0x12);
         assert_eq_hex!(ar.get(), 0x1200);
 
+        // lo bit
         ar.external_write(0x34);
         assert_eq_hex!(ar.get(), 0x1234);
 
         ar.set(0x0123);
         assert_eq_hex!(ar.get(), 0x0123);
+
+        ar.increment(1);
+        assert_eq_hex!(ar.get(), 0x0124);
+
+        ar.increment(0x20);
+        assert_eq_hex!(ar.get(), 0x0144);
+
+        ar.set(0x3FFF);
+        assert_eq_hex!(ar.get(), 0x3FFF);
+
+        ar.increment(1);
+        assert_eq_hex!(ar.get(), 0, "increment should wrap around due to mirroring");
+    }
+
+    #[test]
+    fn test_addr_register_special_cases() {
+        let mut ar = AddrRegister::new();
+        assert_eq_hex!(ar.get(), 0);
+
+        ar.external_write(0x50);
+        assert_eq_hex!(ar.get(), 0x1000, "hi bit should wrap due to mirroring");
+
+        ar.external_write(0xFF);
+        assert_eq_hex!(ar.get(), 0x10FF, "should modify lo bit");
+
+        ar.external_write(0xBC);
+        assert_eq_hex!(
+            ar.get(),
+            0x3CFF,
+            "should be hi bit, wrapped due to mirroring"
+        );
+
+        ar.reset_to_hi_bit();
+        ar.external_write(0xAB);
+        assert_eq_hex!(ar.get(), 0x2BFF, "should be hi bit again, after reset");
     }
 }
